591fac1f950db06fb4362bd806a536639aaa815e
Marcin Kaczynski <M.Kaczynski@stud.elka.pw.edu.pl>
2012-04-21 14:57:42 +0200

    Inicjalizacja projektu.
    Dodany podstawowy projekt utworzony w NetBeans. W projekcie
    jest przykladowe okno glowne dla programu.

---
+++ src/neutron/GUI/Neutron.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package neutron.GUI;
+
+/**
+ *
+ * @author Marcin
+ */
+public class Neutron {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainWindow mw = new MainWindow();
+        mw.setVisible(true);
+    }
+}
