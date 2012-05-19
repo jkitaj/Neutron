0cc039c54c342ff295b642bac85e16a6389e557b
Marcin Kaczynski <M.Kaczynski@stud.elka.pw.edu.pl>
2012-05-19 16:32:18 +0200

    Dodane pliki klas reprezentujacych ruch w jednym z dostepnych kierunkow.

---
+++ src/neutron/Logic/Model/Moves/WMove.java
@@ -0,0 +1,18 @@
+package neutron.Logic.Model.Moves;
+
+import neutron.Logic.Interfaces.BorderElementType;
+import neutron.Logic.Interfaces.IGameBorder;
+import neutron.Logic.Interfaces.IMove;
+import neutron.Utils.Position;
+
+/**
+ * @author Marcin
+ */
+public class WMove implements IMove {
+
+    @Override
+    public IGameBorder Move(IGameBorder border, BorderElementType type, Position pos) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
