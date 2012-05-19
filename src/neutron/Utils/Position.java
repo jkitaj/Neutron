e818b0c0bfed32d8a223983c2260f118aad11f53
Marcin Kaczynski <M.Kaczynski@stud.elka.pw.edu.pl>
2012-05-19 15:44:28 +0200

    Dodana klasa opisujaca pozycje.

---
+++ src/neutron/Utils/Position.java
@@ -0,0 +1,24 @@
+package neutron.Utils;
+
+/**
+ *
+ * @author Marcin
+ */
+public class Position {
+    
+    private int x;
+    private int y;
+    
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public int getX() {
+        return x;
+    }
+    
+    public int getY() {
+        return y;
+    }
+}
