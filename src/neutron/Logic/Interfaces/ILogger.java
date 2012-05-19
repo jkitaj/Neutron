295138d5d3e68dfcd7a18d67dd457887109cd444
Marcin Kaczynski <M.Kaczynski@stud.elka.pw.edu.pl>
2012-05-19 14:40:49 +0200

    Przypadkowo pominiety plik z interfejsem loggera.

---
+++ src/neutron/Logic/Interfaces/ILogger.java
@@ -0,0 +1,9 @@
+package neutron.Logic.Interfaces;
+
+/**
+ * Simple logger to log information about calculations.
+ * @author Marcin
+ */
+public interface ILogger {
+    void writeMessage(String message);
+}
